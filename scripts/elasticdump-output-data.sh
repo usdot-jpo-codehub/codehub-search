6eb27960afe547731aed746b95f97ee62e0eb3a1
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-11-12 16:48:56 -0500

    Improve scripts files with more instruction

--- scripts/elasticdump-output-data.sh
+++ scripts/elasticdump-output-data.sh
@@ -3,6 +3,8 @@
 # This script gives an example of how to use the locally-installed elasticsearch-dump 
 # to pull data from the repos index and write it to a file called repos-data.json
 
+# Note: Swap the --input and --output destinations to upload data from repos-data.json into the ES repos index
+
 if [ -z "$ELASTICSEARCH_URL" ]; then
     echo "Error: Required environment variable ELASTICSEARCH_URL not set."
     exit 1
--- scripts/list-indexes.sh
+++ scripts/list-indexes.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+if [ -z "$ELASTICSEARCH_URL" ]; then
+    echo "Error: Required environment variable ELASTICSEARCH_URL not set."
+    exit 1
+fi
+
 curl $ELASTICSEARCH_URL/_cat/indices?v
